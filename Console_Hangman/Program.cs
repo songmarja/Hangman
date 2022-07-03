621e59ed59bf08fa4cd579ac1aa21882df1dfc28
songmarja <57094009+songmarja@users.noreply.github.com>
2022-07-03 20:00:25 +0200

    Add project files.

---
+++ Console_Hangman/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Text;
+using static Console_Hangman.Services;
+
+namespace Console_Hangman
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Printing header and some text about the game to the player
+            GetHeader();
+
+            bool userWantsToPlay = true;
+            while (userWantsToPlay)
+            {
+                // Stores all wrong guessed letters
+                StringBuilder wrongLetters = new StringBuilder();
+
+                //Check if user wants to play
+                userWantsToPlay = ReadyToPlay();
+
+                // Gets the random, secret word from string array and
+                //  "Convert" the string into a char array
+                //string[] secretWords = GetSecretWordsFromFile();
+                char[] secretChars = GetSecretRandomWordFromFile();
+
+                //Show the secret word by lower dashes for user
+                char[] guessedChars = ShowSecretWordWithDashes(secretChars);
+
+                // Start the game -loop, with first guess, and until user wants to quit - OR - finds the word - OR - user has made 10 guesses
+                bool correctGuess = false;
+                bool notTooManyGuesses = true;
+                
+                int guessCount = 0;
+                while (!correctGuess && notTooManyGuesses)
+                {
+                    string inputLetters = ReadUserInput(10 - guessCount);
+
+                    if (inputLetters.Length >= 2)
+                    {
+                        guessCount++;
+                        // Compare input with secret word(in char[])
+                        bool equalWords = CheckIfIsEqual(secretChars, inputLetters);
+                        if (equalWords == true)
+                        {
+                            Console.WriteLine($"**** CONGRATULATION! You found out the word! **** ");
+                            //CreateUserMessage();
+                            correctGuess = true;
+                        }
+                        // It was not the secret word
+                        else
+                        {
+                            if (guessCount >= 10)
+                            {
+                                Console.WriteLine("**** You missed it! You have used your 10 guesses. ****");
+                                notTooManyGuesses = false;
+                                //CreateUserMessage();
+                                //TODO***** Validation, if want to play another game..
+                            }
+                            Console.WriteLine("**** Sorry, not the word I think about, but try again! ****");
+                        }
+                    }
+                    else // = one character guess
+                    {
+                        bool letterNotFound = true;
+                        for (int i = 0; i < secretChars.Length; i++)
+                        {
+                            // Found the letter in the secret word
+                            if (secretChars[i] == inputLetters[0])
+                            {
+                                letterNotFound = false;
+                            }
+                        }
+
+                        // when not found letter in secret word
+                        if (letterNotFound == true)
+                        {
+                            //Is it first guess on that letter?
+                            bool firstGuess = FirstGuess(wrongLetters, inputLetters);
+                            if (firstGuess == true)
+                            {
+                                guessCount++;
+                            }
+                            Console.WriteLine("\n**** Noop. Not a letter in the secret word. Try again. ****");
+                        }
+                        // letter found
+                        else
+                        {
+                            guessedChars = UpdateGuessedChars(guessedChars, secretChars, inputLetters[0]);
+                            
+                            correctGuess = CorrectWordGuess(secretChars, guessedChars);
+                            if(correctGuess == true)
+                            {
+                                Console.WriteLine($"**** CONGRATULATION! You found out the word! ****");
+                                correctGuess = true;
+                                //Console.WriteLine("Do you want to play another game? y (yes) /n (no): ");
+                            }
+                            guessCount++;
+                        }
+
+                        if (guessCount >= 10)
+                        {
+                            Console.WriteLine("\n**** SORRY! You missed it! You have used your 10 guesses. ****");
+                            notTooManyGuesses = false;
+                            //TODO***** Validation, if want to play another game..
+                        }
+
+                        foreach (char letter in guessedChars)
+                        {
+                            Console.Write($"{letter} ");
+                        }
+                        Console.Write($"\n" +
+                            $"You have tried this characters: {wrongLetters.ToString()}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Console_Hangman/Services.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.IO;
+
+namespace Console_Hangman
+{
+    public  static class Services
+    {
+       
+        public static void GetHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("***********************************************************************************************************************");
+            Console.WriteLine("\n                                                    Welcome to play Hangman!\n");
+            Console.WriteLine("***********************************************************************************************************************");
+            Console.ResetColor();
+
+            string s = "Hangman game is about guessing a secret word. You have 10 guesses to find out!\n" +
+                       "You can choose to make a guess on the whole word anytime you want,\n" +
+                       "but if you have found some characters, it would be more easy.\n" +
+                       "Good Luck!!";
+            Console.WriteLine(s);
+        }
+        /// <summary>
+        /// Gets an answer from user if hen wants to play. otherwise app turns down
+        /// </summary>
+        /// <returns></returns>
+        public static bool ReadyToPlay()
+        {
+            Console.WriteLine();
+            Console.Write("Are you ready to play? Y (Yes) /N (No): ");
+            string? yesOrNo = null;
+            bool notValid = true; 
+            // Check if valid input. Output => 'y' or 'n'
+            while(notValid)
+            {
+                yesOrNo = Console.ReadLine().ToLower();
+                if (String.IsNullOrEmpty(yesOrNo) || (yesOrNo != "n" && yesOrNo != "y") )
+                {
+                    Console.Write("Please enter: y (Yes) / n (No): \n ");
+                   
+                }
+                else
+                {
+                    notValid = false;
+                }
+            }
+            // Wants to quit
+            if(yesOrNo == "n")
+            {
+                Environment.Exit(0);
+            }
+            // wants to play
+            return true;
+        }
+
+
+        /// <summary>
+        ///  Creates a new random word from the string array, by selecting an index of the string array
+        /// save the word in a char array, and returns it.
+        /// </summary>
+        /// <returns>string word</returns>
+        public static char[] GetSecretRandomWordFromFile()
+        {
+            Random rnd = new Random();
+
+            // string[] randomWords = { "solsting", "myggmedel", "sommarlov", "pollenallergi",
+            //    "smultron","åskväder","semester", "brandrisk","vattentemperatur", "kanadensare", "badring", "glass", "bajamaja"};
+
+            string[] randomWords = GetSecretWordsFromFile();
+            int index = rnd.Next(0, randomWords.Length);
+            char[] secretChars = randomWords[index].ToCharArray();
+
+            return secretChars;
+        }
+
+        public static string[] GetSecretWordsFromFile()
+        {
+            string[] secretWords = new string[17];
+            string? secretWordsString = null;
+            string secretWordsFile = "../../../secretWords.txt";
+
+            try
+            {
+                secretWordsString = File.ReadAllText(secretWordsFile);
+               
+                secretWords = secretWordsString.Split(',')
+                    .Select(x => x.Trim())
+                    .Where(x => !string.IsNullOrWhiteSpace(x))
+                    .ToArray();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was an error, when reading a file",e);
+            }
+            return secretWords;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static string ReadUserInput(int guessesLeft)
+        {
+            //Console.Clear();
+            Console.Write($"\n\nYou have now: {guessesLeft} guesses left.\nWrite a letter you think is a part of the word, or guess the word: ");
+            string? input = Console.ReadLine();
+            while (String.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Enter a word or a character you think is correct: ");
+                input = Console.ReadLine();
+            }
+            return input;
+        }
+
+
+
+        public static bool CheckIfIsEqual(char[] secretChars, string inputLetters)
+        {
+            bool isEqual = secretChars.SequenceEqual(inputLetters.ToCharArray());
+            return isEqual;
+        }
+
+        public static bool CorrectWordGuess(char[] secretChars, char[] guessedChars)
+        {
+            bool isEqual = secretChars.SequenceEqual(guessedChars);
+
+            return isEqual;
+        }
+
+
+        public static char[] ShowSecretWordWithDashes(char[] letters)
+        {
+            char[] guessedChars = new char[letters.Length];
+            int indx = 0;
+
+            //TODO*********** Comment out show secretChars, when playing
+            foreach (char letter in letters)
+            {
+                Console.Write($"{letter} -  ");
+            }
+            Console.WriteLine();
+
+            foreach (char letter in guessedChars)
+            {
+                Console.Write("_  ");
+                guessedChars[indx] = '_';
+                indx++;
+            }
+            return guessedChars;
+
+        }
+        
+        public static string ThrowIfNullOrEmpty(this string value, string name)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+            if (value == "")
+            {
+                throw new ArgumentException("Argument must not be the empty string.",
+                                            name);
+            }
+            return value;
+        }
+
+        public static StringBuilder AddGuessedLetter(string inputLetter)
+        {
+            StringBuilder guessedLetters = new StringBuilder();
+            guessedLetters.Append(inputLetter);
+            return guessedLetters;
+        }
+
+        public static bool FirstGuess(StringBuilder sb, string input)
+        {
+;            bool firstGuess = true;
+            for(int i = 0; i < sb.Length; i++)
+            {
+                if (sb.ToString().Contains(input))
+                {
+                    firstGuess = false;
+                }
+            }
+            sb.Append($"{input}  ");
+            return firstGuess;
+        }
+
+        public static char[] UpdateGuessedChars(char[] guessedChars, char[] secretChars,char inputLetter)
+        {
+            for (int i = 0; i < secretChars.Length; i++)
+            {
+                // Found the letter in the secret word
+                if (secretChars[i] == inputLetter)
+                {
+                    guessedChars[i] = inputLetter;
+                }
+               
+            }
+            return guessedChars;
+        }
+    }
+}
